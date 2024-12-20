e9b5d00bbea3ec98adcce3f006ba88d0ae3222f0
Piotr Sarnecki <155118184+psarnecki@users.noreply.github.com>
2024-12-20 01:39:53 +0100

    Utwórz pamięć dzieloną do odebrania wiadomości
    -implementacja funkcji obsługujących tworzenie pamięci dzielonej dla obu programów
    -implementacja wysyłania i odbierania wiadomości Dziekana
    -dodanie mutexu do oczekiwania na wiadomość (Student)
    -dodanie obsługi błędów dla funkcji systemowych
    -zaimplementowanie funkcji czyszczącej pamięć dzieloną i zwalniającej wszystkie zasoby po zakończeniu pracy programu

--- Dziekan.c
+++ Dziekan.c
@@ -2,8 +2,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h> // do obsługi pamięci dzielonej
+
+#define SEM_SIZE sizeof(int)
+
+int sem_id, shm_id;
+int *shared_mem = NULL;
+
+void cleanup();
 
 int main() {
-   
+    srand(time(NULL)); // Inicjalizacja generatora liczb pseudolosowych
+
+   key_t key = ftok(".", 'S');
+	if (key == -1){
+		perror("Blad tworzenia klucza!");
+		exit(-1);
+	} else
+		//printf("key: %d\n", key);
+
+    shm_id = shmget(key, SEM_SIZE, IPC_CREAT | 0666);
+	if(shm_id == -1){
+		perror("Blad tworzenia segmentu pamieci dzielonej!");
+		exit(-1);
+	} else
+		//printf("shm_id: %d\n", shm_id);
+
+    shared_mem = (int *) shmat(shm_id, NULL, 0);
+	if(shared_mem == (int *)(-1)){
+		perror("Blad przylaczenia pamieci dzielonej!");
+		exit(-1);
+	} else {
+    	//printf("Pamiec dzielona przypisana poprawnie!\n");
+    }
+
+    int kierunek = rand() % 10 + 1; // Kierunki od 1 do 10
+    printf("Dziekan ogłasza: Kierunek %d pisze egzamin.\n", kierunek);
+
+    *shared_mem = kierunek; // Zapisanie numeru kierunku do pamięci współdzielonej
+
+    printf("Dziekan zakończył działanie.\n");
+
+    //cleanup();
     return 0;
+}
+
+void cleanup()
+{
+	if (shared_mem != NULL && shmdt(shared_mem) == -1) {
+        perror("Blad odlaczania pamieci dzielonej");
+    }
+    if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+        perror("Blad usuwania segmentu pamieci dzielonej");
+    }
 }
--- Student.c
+++ Student.c
@@ -3,9 +3,18 @@
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h> // do obsługi pamięci dzielonej
 
 #define MIN_STUDENTS 80
 #define MAX_STUDENTS 160
+#define SEM_SIZE sizeof(int)
+
+int sem_id, shm_id;
+int ogloszony_kierunek = 0;
+int *shared_mem = NULL;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // Argument wątku przekazywany do funkcji wątku przy rozpoczęciu jego wykonywania (struktura)
 typedef struct {
@@ -23,12 +32,57 @@ void* symuluj_przybycie(void* arg) {
         printf("Student %d z kierunku %d przybył do kolejki.\n", i, dane->kierunek);
     }
 
+    // Oczekiwanie na informację od dziekana
+    while (1) {
+        pthread_mutex_lock(&mutex); // Zablokowanie mutexu, aby uzyskać wyłączny dostęp do zmiennej
+        if (ogloszony_kierunek != 0) {
+            pthread_mutex_unlock(&mutex); // Odblokowanie mutexu i wyjście z pętli, jeśli dziekan ogłosił kierunek
+            break;
+        }
+        pthread_mutex_unlock(&mutex); // Odblokowanie mutexu, jeśli dziekan nie ogłosił kierunku aby inne wątki kierunków mogły kontuunować działanie
+        sleep(1); // 1 sekunda przerwy przed ponownym sprawdzeniem ogłoszenia w pętli 
+    }
+
+    // Sprawdzenie ogłoszonego kierunku
+    if (dane->kierunek == ogloszony_kierunek) {
+        printf("Studenci z kierunku %d wchodzą na egzamin.\n", dane->kierunek);
+    } else {
+        printf("Studenci z kierunku %d wracają do domu.\n", dane->kierunek);
+    }
+
     pthread_exit(NULL);
 }
+
+void cleanup();
 
 int main() {
     srand(time(NULL)); // Inicjalizacja generatora liczb pseudolosowych
 
+    key_t key = ftok(".", 'S');
+	if (key == -1){
+		perror("Blad tworzenia klucza!");
+		
+		exit(-1);
+	} else
+		//printf("key: %d\n", key);
+
+    shm_id = shmget(key, SEM_SIZE, IPC_CREAT | 0666);
+	if(shm_id == -1){
+		perror("Blad tworzenia segmentu pamieci dzielonej!");
+		
+		exit(-1);
+	} else
+		//printf("shm_id: %d\n", shm_id);
+
+    shared_mem = (int *) shmat(shm_id, NULL, 0);
+	if(shared_mem == (int *)(-1)){
+		perror("Blad przylaczenia pamieci dzielonej!");
+		
+		exit(-1);
+	} else {
+    	//printf("Pamiec dzielona przypisana poprawnie!\n");
+    }
+    
     int liczba_kierunkow = rand() % 11 + 5; // Losowanie liczby kierunków z zakresu 5-15
     printf("Liczba kierunków: %d\n", liczba_kierunkow);
 
@@ -52,6 +106,11 @@ int main() {
         }
     }
 
+    ogloszony_kierunek = *shared_mem;
+    printf("\nOdebrany ogłoszony kierunek: %d\n", ogloszony_kierunek);
+
+    cleanup();
+
     // Czekanie na zakończenie wszystkich wątków
     for (int i = 0; i < liczba_kierunkow; i++) {
         pthread_join(watki[i], NULL);
@@ -60,4 +119,14 @@ int main() {
     printf("\nSymulacja zakończona.\n");
     
     return 0;
+}
+
+void cleanup()
+{
+	if (shared_mem != NULL && shmdt(shared_mem) == -1) {
+        perror("Blad odlaczania pamieci dzielonej");
+    }
+    if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+        perror("Blad usuwania segmentu pamieci dzielonej");
+    }
 }
